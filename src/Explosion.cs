5a28120b42684604a0772ef3348c8a01722f1a67
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2024-09-14 22:23:15 +0900

    Explosion Scene の追加

---
+++ src/Explosion.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class Explosion : Sprite2D
+{
+    private const int PATTERNS_COUNT = 37; // パターン数
+    private const int PATTERNS_IN_SEC = 37 * 2; // 1秒間に表示するパターン数
+
+    // 経過時間
+    private float _elapsedTime = 0;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+        _elapsedTime += (float)delta;
+        Frame = (int)(_elapsedTime * PATTERNS_IN_SEC);
+
+        if (Frame > PATTERNS_COUNT) {
+            QueueFree();
+        }
+	}
+}
