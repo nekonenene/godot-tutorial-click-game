3af57b77bad78075fe25fd2b82ccc9184151478d
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2024-09-14 23:09:01 +0900

    ゲームエンド後に余分なループを回さない

--- Main.cs
+++ Main.cs
@@ -5,6 +5,7 @@ using System.Linq;
 public partial class Main : Node2D
 {
     private Label _label;
+    private Boolean _isGameEnd = false;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -16,11 +17,17 @@ public partial class Main : Node2D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        if (_isGameEnd)
+        {
+            return;
+        }
+
         // Tako という文字が含まれるノードの個数を取得
         int takoCount = GetChildren().Where(node => node.Name.ToString().Contains("Tako")).Count();
         if (takoCount == 0)
         {
             _label.Visible = true;
+            _isGameEnd = true;
         }
     }
 }
