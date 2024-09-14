25ed0b5c02f621ca7b82bfa4512d56df4e0544b7
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2024-09-14 22:07:01 +0900

    全てのたこ焼きを消したときに「Game Clear」の文字を表示

---
+++ Main.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+using System.Linq;
+
+public partial class Main : Node2D
+{
+    private Label _label;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        _label = GetNode<Label>("ClearLabel");
+        _label.Visible = false;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+        // Tako という文字が含まれるノードの個数を取得
+        int takoCount = GetChildren().Where(node => node.Name.ToString().Contains("Tako")).Count();
+        if (takoCount == 0)
+        {
+            _label.Visible = true;
+        }
+	}
+}
